06e97eeec2123c572412acc49ec1032832a8efbd
ArazAhmet <aa227xv@student.lnu.se>
2025-10-21 01:12:23 +0200

    Fix module import path for code-metric-analyzer package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { analyzeRepository } from './node_modules/code-metric-analyzer/codeMetric.js'
+import { analyzeRepository } from 'code-metric-analyzer'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
